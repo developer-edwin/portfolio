571e3e2aacda28e18d113c86ab0f9822a33b105f
Edwin Gonzalez <developer.edwin.gonzalez@gmail.com>
2022-06-22 20:27:55 -0500

    Fix linter JS issues

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
+/* eslint-disable no-restricted-globals */
+function toggleManu() {
+  document.querySelector('nav').classList.toggle('displayMenu');
+  document.querySelector('body').classList.toggle('noScroll');
+  document.querySelector('#logo').classList.toggle('blur');
+  document.querySelector('section').classList.toggle('blur');
+}
+
 document.querySelector('#hamburgerMenu').addEventListener('click', () => {
-    toggleManu()
+  toggleManu();
 });
 
 document.querySelector('#hamburgerMenuClose').addEventListener('click', () => {
-    toggleManu()
+  toggleManu();
 });
 
 if (screen.width < 768) {
-    const navLinks = document.querySelectorAll('.navLink');
-    for (let i = 0; i < navLinks.length; i++) {
-        navLinks[i].addEventListener('click', () => {
-            toggleManu()
-        });
-    }
-}
-
-function toggleManu(justRemove = 0) {
-    document.querySelector('nav').classList.toggle('displayMenu');
-    document.querySelector('body').classList.toggle('noScroll');
-    document.querySelector('#logo').classList.toggle('blur');
-    document.querySelector('section').classList.toggle('blur');
+  const navLinks = document.querySelectorAll('.navLink');
+  for (let i = 0; i < navLinks.length; i += 1) {
+    navLinks[i].addEventListener('click', () => {
+      toggleManu();
+    });
+  }
 }
